91a36809973717e07d33ad0a30f0902502ac6c95
tuanthanh <marbled.thanh.tt@gmail.com>
2022-09-06 16:20:18 +0700

    demo

---
+++ src/components/Calendar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <Fullcalendar 
+            :options="calendarPlugins"
+            :select="handleSelect"
+            
+        /> 
+    </div>
+</template>
+<script>
+
+import Fullcalendar from "@fullcalendar/vue";
+import DayGridPlugin from "@fullcalendar/daygrid";
+import TimeGridPlugin from "@fullcalendar/timegrid";
+import InteractionPlugin from "@fullcalendar/interaction";
+import ListPlugin from "@fullcalendar/list";
+import { mapGetters } from "vuex";
+export default {
+    name: "CalenDar",
+    components: {
+        Fullcalendar
+    },
+    data() {
+        return {
+            calendarPlugins: {
+                plugins : [
+                DayGridPlugin,
+                TimeGridPlugin,
+                InteractionPlugin,
+                ListPlugin
+                ],
+                headerToolbar: { right: 'dayGridMonth,timeGridWeek,timeGridDay', center: 'title',left:'prev,next today', },
+                selectable: true,
+                weekends: false,
+                events: "EVENTS",
+            },
+            computed: {
+                ...mapGetters(["EVENTS"])
+            },
+            methods: {
+                handleSelect(arg){
+                    console.log(arg)
+                }
+            },
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
