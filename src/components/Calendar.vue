6d93b77e5c4b2c4e5011b25f2c9503ffa7685d52
tuanthanh <marbled.thanh.tt@gmail.com>
2022-09-07 14:22:59 +0700

    add event

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,20 +1,23 @@
 <template>
-    <div>
-        <Fullcalendar 
+    <div class='demo-app'>
+    <div class='demo-app-main'>
+       <Fullcalendar 
+            class='demo-app-calendar'
             :options="calendarPlugins"
-            :select="handleSelect"
-            
-        /> 
+        > 
+      </FullCalendar>
     </div>
+  </div>
 </template>
 <script>
-
 import Fullcalendar from "@fullcalendar/vue";
 import DayGridPlugin from "@fullcalendar/daygrid";
 import TimeGridPlugin from "@fullcalendar/timegrid";
 import InteractionPlugin from "@fullcalendar/interaction";
 import ListPlugin from "@fullcalendar/list";
-import { mapGetters } from "vuex";
+import resourceTimelineDayPlugin from "@fullcalendar/resource-timeline";
+import { createEventId } from "../event/event-utils";
+import { mapActions } from "vuex";
 export default {
     name: "CalenDar",
     components: {
@@ -27,21 +30,61 @@ export default {
                 DayGridPlugin,
                 TimeGridPlugin,
                 InteractionPlugin,
-                ListPlugin
+                ListPlugin,
+                resourceTimelineDayPlugin
                 ],
-                headerToolbar: { right: 'dayGridMonth,timeGridWeek,timeGridDay', center: 'title',left:'prev,next today', },
+                headerToolbar: { 
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay', 
+                    center: 'title',  
+                    left:'prev,next today', 
+                },
+                // initialEvents: INITIAL_EVENTS,
                 selectable: true,
+
+                dayMaxEvents: true,
                 weekends: false,
-                events: "EVENTS",
+                selectMirror: true,
+                select: this.handleDateSelect,
+                eventClick: this.handleEventClick,
+                eventsSet: this.handleEvents,
+                editable: true,
+                droppable: true 
             },
-            computed: {
-                ...mapGetters(["EVENTS"])
-            },
-            methods: {
-                handleSelect(arg){
-                    console.log(arg)
-                }
-            },
+            currentEvents:[]
+        }
+    },
+    methods: {
+        ...mapActions([
+            'updateEvent'
+        ]),
+        handleWeekendsToggle() {
+             this.calendarPlugins.weekends = !this.calendarPlugins.weekends // update a property
+         },
+
+        handleDateSelect(selectInfo) {
+            let title = prompt('Please enter a new title for your event')
+            let calendarApi = selectInfo.view.calendar
+            calendarApi.unselect() // clear date selection
+            if (title) {
+                    calendarApi.addEvent({
+                    id: createEventId(),
+                    title,
+                    start: selectInfo.start,
+                    end: selectInfo.end,
+                    allDay: selectInfo.allDay
+                })
+            }
+        },
+        handleEventClick(clickInfo) {
+            if(confirm(`'xoa di dung ngai' '${clickInfo.event.title}'`)){
+                clickInfo.event.remove()
+            }
+        },
+        handleEvents(events) {
+            this.currentEvents = events
+        },
+        onEventDrop({event}){
+            return this.updateEvent(event)
         }
     }
 }
